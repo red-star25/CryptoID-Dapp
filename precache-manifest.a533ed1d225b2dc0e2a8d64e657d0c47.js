8f9998d2d8e8c2730c5ed6739c77a0e8aa3230d6
red-star25 <nakumdhruv123@gmail.com>
2020-09-20 10:37:19 +0530

    Updates

---
+++ precache-manifest.a533ed1d225b2dc0e2a8d64e657d0c47.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cea4786cc68e6a082b76a563d678ef25",
+    "url": "/CryptoID-Dapp/index.html"
+  },
+  {
+    "revision": "e7ff70b305e618b7e25b",
+    "url": "/CryptoID-Dapp/static/css/main.2a07579f.chunk.css"
+  },
+  {
+    "revision": "d3ce21eb8de305af4bef",
+    "url": "/CryptoID-Dapp/static/js/2.7714c244.chunk.js"
+  },
+  {
+    "revision": "781c78cc4c53c723bbdad1fe42e7a08c",
+    "url": "/CryptoID-Dapp/static/js/2.7714c244.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7ff70b305e618b7e25b",
+    "url": "/CryptoID-Dapp/static/js/main.55644206.chunk.js"
+  },
+  {
+    "revision": "e01bf6fc6661f854515a",
+    "url": "/CryptoID-Dapp/static/js/runtime-main.b9837bb4.js"
+  },
+  {
+    "revision": "cb36b4daf236e61b2b91340c761df631",
+    "url": "/CryptoID-Dapp/static/media/back.cb36b4da.jpg"
+  },
+  {
+    "revision": "08899b7b91c9589872a32a0dca7433ec",
+    "url": "/CryptoID-Dapp/static/media/back1.08899b7b.jpg"
+  }
+]);
